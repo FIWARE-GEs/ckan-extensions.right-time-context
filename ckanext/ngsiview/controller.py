cec7a1ba1ae9bdb325f0a1962c6ce02136010916
Álvaro Arranz <aarranz@ficodes.com>
2018-07-05 21:56:51 +0200

    Code clean up

--- ckanext/ngsiview/controller.py
+++ ckanext/ngsiview/controller.py
@@ -16,105 +16,103 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with Orion Context Broker. If not, see http://www.gnu.org/licenses/.
 
+import json
 from logging import getLogger
 import urlparse
-from pylons import config
-import requests
-import json
+
 import ckan.logic as logic
 import ckan.lib.base as base
 import ckan.plugins as p
+from pylons import config
+import requests
 
 log = getLogger(__name__)
 
 CHUNK_SIZE = 512
 
 
-def proxy_ngsi_resource(context, data_dict):
-    # Chunked proxy for ngsi resources.
-    resource_id = data_dict['resource_id']
-    log.info('Proxify resource {id}'.format(id=resource_id))
-    resource = logic.get_action('resource_show')(context, {'id': resource_id})
-    verify = config.get('ckan.ngsi.verify_requests', True)
-
-    try:
-
-        headers = {
-            'Accept': 'application/json'
-        }
-
-        if 'oauth_req' in resource and resource['oauth_req'] == 'true':
-            token = p.toolkit.c.usertoken['access_token']
-            headers['X-Auth-Token'] = token
-
-        if resource.get('tenant', '') != '':
-            headers['FIWARE-Service'] = resource['tenant']
-        if resource.get('service_path', '') != '':
-            headers['FIWARE-ServicePath'] = resource['service_path']
-
-        url = resource['url']
-        try:
-            parsedurl = urlparse.urlsplit(url)
-        except:
-            base.abort(409, detail='Invalid URL.')
-
-        if not parsedurl.scheme or not parsedurl.netloc:
-            base.abort(409, detail='Invalid URL.')
-
-        if parsedurl.path.find('/v1/queryContext') != -1:
-            if resource.get("payload", "").strip() == "":
-                details = 'Please add a payload to complete the query.'
-                base.abort(409, detail=details)
-
-            try:
-                json.loads(resource['payload'])
-            except:
-                details = "Payload field doesn't contain valid JSON data."
-                base.abort(409, detail=details)
-
-            headers['Content-Type'] = "application/json"
-            r = requests.post(url, headers=headers, data=resource["payload"], stream=True, verify=verify)
-
-        else:
-            r = requests.get(url, headers=headers, stream=True, verify=verify)
-
-        if r.status_code == 401:
-            if 'oauth_req' in resource and resource['oauth_req'] == 'true':
-                details = 'ERROR 401 token expired. Retrieving new token, reload please.'
-                log.info(details)
-                base.abort(409, detail=details)
-                p.toolkit.c.usertoken_refresh()
-
-            elif 'oauth_req' not in resource or resource['oauth_req'] == 'false':
-                details = 'This query may need Oauth-token, please check if the token field on resource_edit is correct.'
-                log.info(details)
-                base.abort(409, detail=details)
-
-        else:
-            r.raise_for_status()
-            base.response.content_type = r.headers['content-type']
-            base.response.charset = r.encoding
-
-        for chunk in r.iter_content(chunk_size=CHUNK_SIZE):
-            base.response.body_file.write(chunk)
-
-    except ValueError:
-        details = ''
-        base.abort(409, detail=details)
-    except requests.HTTPError:
-        details = 'Could not proxy ngsi_resource. We are working to resolve this issue as quickly as possible'
-        base.abort(409, detail=details)
-    except requests.ConnectionError:
-        details = 'Could not proxy ngsi_resource because a connection error occurred.'
-        base.abort(502, detail=details)
-    except requests.Timeout:
-        details = 'Could not proxy ngsi_resource because the connection timed out.'
-        base.abort(504, detail=details)
-
-
 class ProxyNGSIController(base.BaseController):
 
     def proxy_ngsi_resource(self, resource_id):
+        # Chunked proxy for ngsi resources.
         data_dict = {'resource_id': resource_id}
         context = {'model': base.model, 'session': base.model.Session, 'user': base.c.user or base.c.author}
-        return proxy_ngsi_resource(context, data_dict)
+
+        resource_id = data_dict['resource_id']
+        log.info('Proxify resource {id}'.format(id=resource_id))
+        resource = logic.get_action('resource_show')(context, {'id': resource_id})
+        verify = config.get('ckan.ngsi.verify_requests', True)
+
+        try:
+
+            headers = {
+                'Accept': 'application/json'
+            }
+
+            if 'oauth_req' in resource and resource['oauth_req'] == 'true':
+                token = p.toolkit.c.usertoken['access_token']
+                headers['X-Auth-Token'] = token
+
+            if resource.get('tenant', '') != '':
+                headers['FIWARE-Service'] = resource['tenant']
+            if resource.get('service_path', '') != '':
+                headers['FIWARE-ServicePath'] = resource['service_path']
+
+            url = resource['url']
+            try:
+                parsedurl = urlparse.urlsplit(url)
+            except ValueError:
+                base.abort(409, detail='Invalid URL.')
+
+            if not parsedurl.scheme or not parsedurl.netloc:
+                base.abort(409, detail='Invalid URL.')
+
+            if parsedurl.path.find('/v1/queryContext') != -1:
+                if resource.get("payload", "").strip() == "":
+                    details = 'Please add a payload to complete the query.'
+                    base.abort(409, detail=details)
+
+                try:
+                    json.loads(resource['payload'])
+                except json.JSONDecodeError:
+                    details = "Payload field doesn't contain valid JSON data."
+                    base.abort(409, detail=details)
+
+                headers['Content-Type'] = "application/json"
+                r = requests.post(url, headers=headers, data=resource["payload"], stream=True, verify=verify)
+
+            else:
+                r = requests.get(url, headers=headers, stream=True, verify=verify)
+
+            if r.status_code == 401:
+                if 'oauth_req' in resource and resource['oauth_req'] == 'true':
+                    details = 'ERROR 401 token expired. Retrieving new token, reload please.'
+                    log.info(details)
+                    base.abort(409, detail=details)
+                    p.toolkit.c.usertoken_refresh()
+
+                elif 'oauth_req' not in resource or resource['oauth_req'] == 'false':
+                    details = 'This query may need Oauth-token, please check if the token field on resource_edit is correct.'
+                    log.info(details)
+                    base.abort(409, detail=details)
+
+            else:
+                r.raise_for_status()
+                base.response.content_type = r.headers['content-type']
+                base.response.charset = r.encoding
+
+            for chunk in r.iter_content(chunk_size=CHUNK_SIZE):
+                base.response.body_file.write(chunk)
+
+        except ValueError:
+            details = ''
+            base.abort(409, detail=details)
+        except requests.HTTPError:
+            details = 'Could not proxy ngsi_resource. We are working to resolve this issue as quickly as possible'
+            base.abort(409, detail=details)
+        except requests.ConnectionError:
+            details = 'Could not proxy ngsi_resource because a connection error occurred.'
+            base.abort(502, detail=details)
+        except requests.Timeout:
+            details = 'Could not proxy ngsi_resource because the connection timed out.'
+            base.abort(504, detail=details)
